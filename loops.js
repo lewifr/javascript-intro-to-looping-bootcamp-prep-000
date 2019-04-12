6790d086d4d53079d2043538c8101aab46f0212a
Francis Lewis <francis.lewis@me.com>
2019-04-12 13:59:25 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -18,12 +18,12 @@ function whileLoop(num) {
 
 function doWhileLoop(num) {
   let i = 0
-  
+
   function incrementVariable() {
     i = i + 1;
     return i;
   }
-  
+
   do {
     console.log('I run once regardless.')
   } while (incrementVariable() < num)
